e5c8fab7f8164854f359e0edced7dfec01e7eae7
hwlts <hwlts@qq.com>
2013-08-28 01:30:53 +0800

    KeyboardHandler

--- Alpha/main.cpp
+++ Alpha/main.cpp
@@ -1,28 +1,63 @@
-#include <osg/io_utils>
-#include <osg/PositionAttitudeTransform>
+#include <osg/Switch>
 #include <osgDB/ReadFile>
+#include <osgGA/GUIEventHandler>
 #include <osgViewer/Viewer>
-#include <iostream>
 
-class RotateCallback : public osg::NodeCallback
+class KeyboardHandler : public osgGA::GUIEventHandler
 {
 public:
-	RotateCallback() : _rotateZ(0.0) {}
-
-	virtual void operator() (osg::Node* node, osg::NodeVisitor* nv)
+	virtual bool handle(const osgGA::GUIEventAdapter& ea,
+								osgGA::GUIActionAdapter& aa,
+								osg::Object*, osg::NodeVisitor*)
 	{
-		osg::PositionAttitudeTransform* pat = dynamic_cast<osg::PositionAttitudeTransform*>(node);
+		osgViewer::Viewer* viewer = dynamic_cast<osgViewer::Viewer*> (&aa);
+		
+		if (!viewer)
+			return false;
 
-		if(pat)
+		switch (ea.getEventType())
 		{
-			osg::Quat quat(osg::DegreesToRadians(_rotateZ),osg::Z_AXIS);
-			pat->setAttitude(quat);
-			_rotateZ+=1.0;
+		case osgGA::GUIEventAdapter::KEYDOWN:
+			if (ea.getKey() == osgGA::GUIEventAdapter::KEY_Space)
+			{
+				int width = ea.getWindowWidth();
+				int height = ea.getWindowHeight();
+				viewer->requestWarpPointer(width*0.5,height*0.5);
+			}
+			else
+			{
+				osg::Switch* root = dynamic_cast<osg::Switch*>(viewer->getSceneData());
+				if (!root)
+					return false;
+				
+				if (ea.getKey()=='1')
+				{
+					root->setValue(0,true);
+					root->setValue(1,false);
+				}
+				else if (ea.getKey()=='2')
+				{
+					root->setValue(0,false);
+					root->setValue(1,true);
+				}
+				return true;
+			}
+			break;
+		default:break;
 		}
-
-		traverse(node,nv);
+	return false;
 	}
-
-protected:
-	double _rotateZ;
 };
+
+int main(int argc, char** argv)
+{
+
+	osg::ref_ptr<osg::Switch> root = new osg::Switch;
+	root->addChild(osgDB::readNodeFile("cessna.osg"),true);
+	root->addChild(osgDB::readNodeFile("cessnafire.osg"),false);
+
+	osgViewer::Viewer viewer;
+	viewer.setSceneData(root);
+	viewer.addEventHandler(new KeyboardHandler);
+	return viewer.run();
+}
